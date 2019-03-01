64fe8aef063c9b29a9ec6d300390a2b6ec09c14c
Łukasz Byrka <lukasz.byrka1@gmail.com>
2019-03-01 22:56:34 +0100

    TextBox And RadioButton
    Adding 2 nwe tests.
    TextBox test and Radio Button test

---
+++ RadioButton_CheckBox/StartingPoint.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+class StartingPoint
+{
+    static IWebDriver firefox = new FirefoxDriver();
+    static IWebElement radiobutton;
+
+    static void Main()
+    {
+        string radiobuttonURL = "http://testing.todvachev.com/special-elements/radio-button-test/";
+        string[] button = { "1", "3", "5" };
+        firefox.Navigate().GoToUrl(radiobuttonURL);
+        Console.Clear();
+        for (int i = 0; i < button.Length; i++)
+        {
+            try
+            {
+                radiobutton = firefox.FindElement(By.CssSelector(".entry-content > form:nth-child(2) > p:nth-child(6) > input:nth-child(" + button[i] + ")"));
+            }
+            catch (NoSuchElementException)
+            {
+                RedMassage("I can't see radio button");
+                goto quit;
+            }
+            if (radiobutton.GetAttribute("checked") == "true")
+            {
+                GreenMassage("Radio button " + i + " is Check");
+            }
+            else
+            {
+                RedMassage("Radio button " + i + " is uncheck");
+            }
+        }
+        Thread.Sleep(5000);
+    quit:
+        firefox.Quit();
+    }
+    static private void GreenMassage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    static private void RedMassage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
---
+++ TextBox/StartingPoint.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+class StartingPoint
+{
+    static IWebElement textbox;
+    static IWebDriver firefox = new FirefoxDriver();
+    static void Main()
+    {
+        string textboxURL = "http://testing.todorvachev.com/special-elements/text-input-field/";
+
+        firefox.Navigate().GoToUrl(textboxURL);
+
+        Console.Clear();
+        try
+        {
+            textbox = firefox.FindElement(By.XPath("/html/body/div/div[2]/div/article/div/p[3]/input"));
+            if (textbox.Displayed)
+            {
+                GreenMassage("I can see Text Box element on page");
+            }
+        }
+        catch(NoSuchElementException)
+        {
+            RedMassage("I can't find Text Box element on web page");
+        }
+
+        textbox.SendKeys("testing key");
+        Console.WriteLine(textbox.GetAttribute("value"));
+        Console.WriteLine("Max length : " + textbox.GetAttribute("maxlength"));
+        Thread.Sleep(50000);
+        firefox.Quit();
+    }
+    static private void GreenMassage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+    static private void RedMassage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
